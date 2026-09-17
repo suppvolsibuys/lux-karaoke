a30f75a1d2ca13d6cb0675544894a66a08707c8d
suppvolsibuys <hamrulkamal89@gmail.com>
2026-09-18 03:00:00 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,36 @@
-const CACHE_NAME = 'lux-karaoke-v3';
+const CACHE_NAME = 'lux-karaoke-universal-v1';
 const ASSETS_TO_CACHE = [
-    './karaoke.html',
-    './remote.html',
-    './stage.html',
+    './index.html',
     './manifest.json',
-    'https://cdn.tailwindcss.com',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-    'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js'
+    './sw.js'
 ];
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
     );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) return caches.delete(key);
+                })
+            );
+        })
+    );
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request).catch(() => {
+                return caches.match('./index.html');
+            });
         })
     );
 });
