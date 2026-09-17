e5d64683ca05144404b19804d37dfc73e9690900
suppvolsibuys <hamrulkamal89@gmail.com>
2026-09-18 02:15:01 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'lux-karaoke-v3';
+const ASSETS_TO_CACHE = [
+    './karaoke.html',
+    './remote.html',
+    './stage.html',
+    './manifest.json',
+    'https://cdn.tailwindcss.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
